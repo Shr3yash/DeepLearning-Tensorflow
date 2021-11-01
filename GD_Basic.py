d73e3278248346f5d014a79996ce21482ee20011
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-11-01 09:00:15 +0530

    Create GD_Basic.py

---
+++ GD_Basic.py
@@ -0,0 +1,18 @@
+import numpy as np
+def gradient_descent(x,y):
+    m_curr=b_curr=0
+    iterations = 1000
+    learning_rate = 0.08
+    n = len(x)
+    for i in range (iterations):
+        y_predicted = m_curr*x + b_curr
+        cost = (1/n)*sum([val**2 for val in (y-y_predicted)])
+        md = -(2/n)*sum(x*(y-y_predicted))
+        bd = -(2/n)*sum(y-y_predicted)
+        m_curr = m_curr - learning_rate*md
+        b_curr = b_curr - learning_rate*bd
+        print("m {},b {},cost {}, iteration  {}".format(m_curr,b_curr,cost, i))
+x = np.array([1,2,3,4,5])
+y = np.array([5,7,9,11,13])
+
+gradient_descent(x,y)
