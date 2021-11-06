5c0f91dd0f8bad7ac51f6b211142038c5cdcb0b8
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-11-06 09:37:41 +0530

    Create mnistninetypc.py

---
+++ mnistninetypc.py
@@ -0,0 +1,52 @@
+"""
+following script should train a TensorFlow model on the fashion MNIST 
+dataset to ~90% test accuracy.
+It'll save the model to the current directory using the ".h5" extension.
+"""
+import tensorflow as tf
+from tensorflow.keras import datasets, layers
+
+
+print(tf.__version__)
+
+# Get data
+(train_images, train_labels), (test_images, test_labels) = datasets.fashion_mnist.load_data()
+
+# Normalize images (get values between 0 & 1)
+train_images, test_images = train_images / 255.0, test_images / 255.0 
+
+# Check shape of input data
+# print(train_images.shape)
+# print(train_labels.shape)
+
+# Build model
+model = tf.keras.Sequential([
+    # Reshape inputs to be compatible with Conv2D layer
+    layers.Lambda(lambda x: tf.expand_dims(x, axis=-1)),
+    layers.Conv2D(32, 3, activation="relu"),
+    layers.MaxPool2D(),
+    layers.Conv2D(32, 3, activation="relu"),
+    layers.MaxPool2D(),
+    layers.Conv2D(32, 3, activation="relu"),
+    layers.Flatten(), # flatten outputs of final Conv layer to be suited for final Dense layer
+    layers.Dense(10, activation="softmax")
+])
+
+# Compile model 
+model.compile(loss="sparse_categorical_crossentropy", # if labels aren't one-hot use sparse (if labels are one-hot, drop sparse)
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=["accuracy"])
+
+# Fit model
+print("Training model...")
+model.fit(x=train_images,
+          y=train_labels,
+          epochs=10,
+          validation_data=(test_images, test_labels))
+
+# Evaluate model 
+print("Evaluating model...")
+model.evaluate(test_images, test_labels)
+
+# Save model to current working directory
+model.save("test_image_model.h5")
