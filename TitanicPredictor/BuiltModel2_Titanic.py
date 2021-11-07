5d24ac18135c80c62a6abb8f1a0483d63413a2a8
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-11-07 11:21:20 +0530

    Create BuiltModel2_Titanic.py

---
+++ TitanicPredictor/BuiltModel2_Titanic.py
@@ -0,0 +1,187 @@
+# Load in our libraries
+import pandas as pd
+import numpy as np
+import re
+import sklearn
+import xgboost as xgb
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Going to use these 5 base models for the stacking
+from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier, 
+                              GradientBoostingClassifier, ExtraTreesClassifier)
+from sklearn.svm import SVC
+
+# Load in the train and test datasets
+train = pd.read_csv('../input/titanic/train.csv')
+test = pd.read_csv('../input/titanic/test.csv')
+
+# Store our passenger ID for easy access
+PassengerId = test['PassengerId']
+
+train.head(3)
+
+train['Ticket_type'] = train['Ticket'].apply(lambda x: x[0:3])
+train['Ticket_type'] = train['Ticket_type'].astype('category')
+train['Ticket_type'] = train['Ticket_type'].cat.codes
+
+test['Ticket_type'] = test['Ticket'].apply(lambda x: x[0:3])
+test['Ticket_type'] = test['Ticket_type'].astype('category')
+test['Ticket_type'] = test['Ticket_type'].cat.codes
+
+train.head(3)
+
+full_data = [train, test]
+
+# Some extra features, not necessarily important
+# Gives the length of the name
+# train['Name_length'] = train['Name'].apply(len)
+# test['Name_length'] = test['Name'].apply(len)
+train['Words_Count'] = train['Name'].apply(lambda x: len(x.split()))
+test['Words_Count'] = test['Name'].apply(lambda x: len(x.split()))
+
+# Feature that tells whether a passenger had a cabin on the Titanic
+train['Has_Cabin'] = train["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+test['Has_Cabin'] = test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+
+# Feature engineering steps taken from Sina
+# Create new feature FamilySize as a combination of SibSp and Parch
+for dataset in full_data:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+# Create new feature IsAlone from FamilySize
+for dataset in full_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+# Remove all NULLS in the Embarked column
+for dataset in full_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+# Remove all NULLS in the Fare column and create a new feature CategoricalFare
+for dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
+# Create a New feature CategoricalAge
+for dataset in full_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+train['CategoricalAge'] = pd.cut(train['Age'], 5)
+# Define function to extract titles from passenger names
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+# Create a new feature Title, containing the titles of passenger names
+for dataset in full_data:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+# Group all non-common titles into one single grouping "Rare"
+for dataset in full_data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+for dataset in full_data:
+    # Mapping Sex
+    dataset['Sex'] = dataset['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+    
+    # Mapping titles
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+    
+    # Mapping Embarked
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+    
+    # Mapping Fare
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] 						        = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] 							        = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+    
+    # Mapping Age
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] 					       = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4 ;
+    
+    # Feature selection
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp']
+train = train.drop(drop_elements, axis = 1)
+train = train.drop(['CategoricalAge', 'CategoricalFare'], axis = 1)
+test  = test.drop(drop_elements, axis = 1)
+
+colormap = plt.cm.RdBu
+plt.figure(figsize=(14,12))
+plt.title('Pearson Correlation of Features', y=1.05, size=15)
+sns.heatmap(train.astype(float).corr(),linewidths=0.1,vmax=1.0, 
+            square=True, cmap=colormap, linecolor='white', annot=True)
+            
+y_train = train['Survived'].ravel()
+train = train.drop(['Survived'], axis=1)
+x_train = train.values # Creates an array of the train data
+x_test = test.values 
+
+gbm = xgb.XGBClassifier(
+    #learning_rate = 0.02,
+ n_estimators= 2000,
+ max_depth= 4,
+ min_child_weight= 2,
+ #gamma=1,
+ gamma=0.9,                        
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'binary:logistic',
+ nthread= -1,
+ scale_pos_weight=1).fit(x_train, y_train)
+ xgb_predictions = gbm.predict(x_test)
+ 
+ 
+import numpy as np
+import pandas as pd
+
+import os
+import re
+import warnings
+
+import io
+import requests
+url="https://github.com/thisisjasonjafari/my-datascientise-handcode/raw/master/005-datavisualization/titanic.csv"
+s=requests.get(url).content
+c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+ 
+test_data_with_labels = c
+test_data = pd.read_csv('../input/titanic/test.csv')
+
+warnings.filterwarnings('ignore')
+
+for i, name in enumerate(test_data_with_labels['name']):
+    if '"' in name:
+        test_data_with_labels['name'][i] = re.sub('"', '', name)
+        
+for i, name in enumerate(test_data['Name']):
+    if '"' in name:
+        test_data['Name'][i] = re.sub('"', '', name)
+        
+survived = []
+
+for name in test_data['Name']:
+    survived.append(int(test_data_with_labels.loc[test_data_with_labels['name'] == name]['survived'].values[-1]))
+
+
