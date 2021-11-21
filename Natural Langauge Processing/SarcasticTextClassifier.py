b2308bbceb4e309c96e9efd5dcc00861d8d13afc
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-11-21 08:36:25 +0530

    Create SarcasticTextClassifier.py

---
+++ Natural Langauge Processing/SarcasticTextClassifier.py
@@ -0,0 +1,35 @@
+
+# NLP and sentimental analysis through bigger texts and
+# now while working with bigger data
+
+import wget
+wget.download("https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json")
+
+import json
+
+with open("/tmp/sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+
+sentences = []
+labels = []
+urls = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+print(len(word_index))
+print(word_index)
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post')
+print(padded[0])
+print(padded.shape)
